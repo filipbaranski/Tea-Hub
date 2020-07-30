36186476135c5a50bc2ea67caecd93d49dd88788
filipbaranski <f.g.baranski@gmail.com>
2020-07-30 16:51:02 +0200

    Pre-rebase

---
+++ precache-manifest.c59879e528717ddc3cda3c7be6306394.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2cf6699756fbc8b0ed6",
+    "url": "/Tea-Hub/css/app.0ed2c8db.css"
+  },
+  {
+    "revision": "b2cf6699756fbc8b0ed6",
+    "url": "/Tea-Hub/js/app.7782b2d2.js"
+  },
+  {
+    "revision": "719dbb7c002f3297ee24",
+    "url": "/Tea-Hub/js/chunk-vendors.f3197c2e.js"
+  },
+  {
+    "revision": "f00e8ab7adbbc17654ca76dac9128c37",
+    "url": "/Tea-Hub/img/fav.f00e8ab7.png"
+  },
+  {
+    "revision": "409bd4791041a5518e20ecfbd2dd9c61",
+    "url": "/Tea-Hub/img/bar360.409bd479.png"
+  },
+  {
+    "revision": "9fbfa79d3e91ca849b7021b7b4a8deac",
+    "url": "/Tea-Hub/img/bar.9fbfa79d.png"
+  },
+  {
+    "revision": "464527b2e180b490eda053bc9b8f79ff",
+    "url": "/Tea-Hub/index.html"
+  },
+  {
+    "revision": "f00e8ab7adbbc17654ca76dac9128c37",
+    "url": "/Tea-Hub/fav.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Tea-Hub/robots.txt"
+  }
+];
