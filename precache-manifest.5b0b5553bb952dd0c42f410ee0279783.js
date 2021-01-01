c570cd34f86126633452af8c7c0b5fa9840ad8fd
filipbaranski <f.g.baranski@gmail.com>
2021-01-01 18:47:34 +0100

    Post-rebase

--- precache-manifest.5b0b5553bb952dd0c42f410ee0279783.js
+++ precache-manifest.5b0b5553bb952dd0c42f410ee0279783.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b2cf6699756fbc8b0ed6",
-    "url": "/Tea-Hub/css/app.0ed2c8db.css"
+    "revision": "ffe72febb1229adcfa93",
+    "url": "/Tea-Hub/css/app.ec596f70.css"
   },
   {
-    "revision": "b2cf6699756fbc8b0ed6",
-    "url": "/Tea-Hub/js/app.7782b2d2.js"
+    "revision": "ffe72febb1229adcfa93",
+    "url": "/Tea-Hub/js/app.ca99aaa6.js"
   },
   {
     "revision": "719dbb7c002f3297ee24",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Tea-Hub/img/bar.9fbfa79d.png"
   },
   {
-    "revision": "464527b2e180b490eda053bc9b8f79ff",
+    "revision": "f7dfe2931fcce162d29a0f53e1b8f17c",
     "url": "/Tea-Hub/index.html"
   },
   {
