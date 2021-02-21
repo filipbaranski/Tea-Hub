55840e8456557c049d568273ee38a7677e296815
filipbaranski <f.g.baranski@gmail.com>
2021-02-21 20:40:45 +0100

    Change to LF

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': [2, 4],
-    'no-underscore-dangle': ['error', { "allow": ["_id"] }],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: [2, 4],
+        'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 };
